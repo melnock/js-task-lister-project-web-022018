c279e527b0c51235ed180c247000c20f73343343
github username <github email address>
2018-04-07 15:08:58 -0400

    Done.

--- src/list.js
+++ src/list.js
@@ -1,3 +1,75 @@
+let listId = 0
+let listForm = document.getElementById("create-list-form")
+let allLists = []
 class List {
   // your code here
+  constructor(name){
+    this.name = name
+    this.id = ++listId
+    allLists.push(this)
+  }
+
+  static all(){
+    return allLists
+  }
+
+  tasks(list){
+    Task.all().filter((task)=>(task.listName === list))
+  }
+
+  deleteList(){
+    event.preventDefault()
+    let thisList = String(event.target.parentElement.innerText)
+    let listObj = List.all().find((list) => (list.name === thisList))
+    let tasks = document.getElementById(thisList).getElementsByTagName("li")
+    document.getElementById(thisList).remove()
+    document.querySelectorAll(`option[value=${thisList}]`)[0].remove()
+  }
+
+  newList(){
+    event.preventDefault()
+    let list = document.createElement('div')
+    let taskForm = document.createElement('div')
+    let input = event.target.children[1].value
+    let newList = new List (input)
+
+    console.log(event)
+    list.id = input
+    list.class = "lists"
+    list.innerHTML = `
+    <h2>${input}<button data-title="${input}" class="delete-list"></button></h2>
+    <ul></ul>
+    `
+    listHolder.appendChild(list)
+    list.addEventListener('click', List.prototype.deleteList)
+  }
+  newTaskList(){
+    event.preventDefault()
+    if (!document.getElementById("create-task-form")){
+      let taskForm = document.createElement('div')
+      let input = event.target.children[1].value
+      console.log(event)
+      taskForm.class = "taskForm"
+      taskForm.innerHTML = `<form id="create-task-form">
+      <label for="parent-list">Select List:</label>
+      <select id="parent-list">
+      </select>
+      <label for="new-task-description">Task description:</label>
+      <input required type="text" id="new-task-description" placeholder="description">
+      <label for="new-task-priority">Priority level:</label>
+      <input type="text" id="new-task-priority" placeholder="priority">
+      <input type="submit" value="Create New Task">
+    </form>`
+      listDiv.prepend(taskForm)
+    }
+
+      let formOption = document.createElement('option')
+      formOption.value = event.target.children[1].value
+      formOption.innerHTML = event.target.children[1].value
+      document.getElementById("parent-list").append(formOption)
+      let taskForm = document.getElementById("create-task-form")
+      taskForm.addEventListener('submit', Task.prototype.newTask)
+  }
 }
+listForm.addEventListener('submit', List.prototype.newTaskList)
+listForm.addEventListener('submit', List.prototype.newList)
--- src/task.js
+++ src/task.js
@@ -1,3 +1,35 @@
+let mainContent = document.getElementById("main-content")
+let listDiv = document.getElementById("app-content")
+let listHolder = document.getElementById("app-content").getElementsByClassName("list-holder")[0]
+let all = []
 class Task {
   // your code here
+  constructor(description, priorityLevel, listName){
+    this.description = description
+    this.priorityLevel = priorityLevel
+    this.listName = listName
+    all.push(this)
+  }
+
+  static all(){
+    return all
+  }
+
+  newTask(){
+    event.preventDefault()
+    console.log(event)
+
+    let newTask = new Task(event.target[1].value, event.target[2].value,event.target[0].value)
+
+    let listDivForTask = document.getElementById(newTask.listName).getElementsByTagName("ul")[0]
+    console.log(listDivForTask)
+    let task = document.createElement('li')
+    let input = newTask.description
+    let priority = newTask.priorityLevel
+
+    task.id = newTask.listName
+    task.class = "task"
+    task.innerHTML = `<br><h3>Task Description: </h3> <br>${input}<br><h3>Task Priority: </h3><br>${priority}<br>`
+    listDivForTask.appendChild(task)
+  }
 }
